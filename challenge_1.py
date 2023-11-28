fe43ed68733099390c6f1bead9d400154dab1355
shakurbol <bolakokbol@gmail.com>
2023-11-28 11:58:54 +0300

    first challange done

---
+++ challenge_1.py
@@ -0,0 +1,20 @@
+def convert_to_24_hour(hour, minute, period):
+    # Check if it's 'am'
+    if period == 'am':
+        # If hour is 12 am, convert it to 0
+        if hour == 12:
+            hour = 0
+    else:
+        # If it's 'pm' and not 12 pm, add 12 to the hour
+        if hour != 12:
+            hour += 12
+    # Return the time in 24-hour format with zero-padding
+    return f"{hour:02d}{minute:02d}"
+
+hour = 8
+minute = 30
+period = 'am'
+
+# Convert 12-hour time to 24-hour time
+result = convert_to_24_hour(hour, minute, period)
+print(f"The 24-hour format is: {result}")
